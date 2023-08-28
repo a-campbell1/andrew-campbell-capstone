c972f9dd67928393499ccbe5026d8687dcc5b389
Drew Campbell <a.campbell1@hotmail.com>
2023-08-28 12:05:16 +0100

    picyure added to screen with title

--- src/Pages/MainPage/HomePage.js
+++ src/Pages/MainPage/HomePage.js
@@ -5,6 +5,7 @@ import "./HomePage.scss";
 import Header from "../../Components/Header/Header";
 import capstone from "../../Assets/Logos/capstone-bg.avif";
 import Footer from "../../Components/Footer/Footer";
+import propic from "../../Assets/images/propic.webp";
 
 function HomePage() {
   return (
@@ -15,11 +16,25 @@ function HomePage() {
       <section className="main-body">
         <div className="main-body__container">
           <div className="main-body__background">
-            <img
-              className="background"
-              src={capstone}
-              alt="smokey background"
-            />
+            <section className="productcards__container">
+              <section className="productcards__container-title">
+                <p>Inspiration picture</p>
+              </section>
+              <section className="productcards__container-content">
+                <section className="productcards__container-pro">
+                <img
+                  className="profesional-pic"
+                  src={propic}
+                  alt="looking through a camera lens"
+                />
+                </section>
+              </section>
+              <img
+                className="background"
+                src={capstone}
+                alt="smokey background"
+              />
+            </section>
           </div>
         </div>
       </section>
